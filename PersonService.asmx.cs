95d6b9697dee80514b63f4ea46afb59e2000ad3a
Carlo Spader <carlograna@hotmail.com>
2017-03-21 19:48:39 -0500

    first import

---
+++ PersonService.asmx.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.Script.Services;
+using System.Web.Services;
+
+namespace JsonWebService
+{
+    /// <summary>
+    /// Summary description for PersonService
+    /// </summary>
+    [WebService(Namespace = "http://local.childsupport.org/Person")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class PersonService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+
+        [WebMethod]
+
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json)]
+        public string GetPerson(string ID)
+        {
+            Person person = new Person()
+            {
+                ARP = "AR2CH2GS11111"
+                ,
+                FirstName = "Carlo"
+                ,
+                LastName = "Spader"
+                ,
+                IsActive = true
+                ,
+                Authenticated = true
+            };
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            return js.Serialize(person);
+
+        }
+    }
+
+    public class Person
+    {
+        public string ARP { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public bool IsActive { get; set; }
+        public bool Authenticated { get; set; }
+    }
+}
---
+++ PersonService.cs
@@ -0,0 +1,67 @@
+﻿using System;   
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Script.Services;
+using System.Web.Script.Serialization;
+
+namespace JsonWebService
+{
+    public class CustomerService : IHttpHandler
+    {
+        public bool IsReusable
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+
+        public void ProcessRequest(HttpContext context)
+        {
+            try
+            {
+
+                switch(context.Request.HttpMethod)
+                {
+                    case "POST":
+                        ReturnPerson();
+
+                        break;
+
+                    case "GET":
+                        ReturnPerson();
+
+                        break;
+                }
+
+            }
+            catch(Exception ex)
+            {
+                HttpContext.Current.Response.Write("Invalid web service request" + ex);
+            }
+        }
+
+        public void ReturnPerson()
+        {
+            Person person = new Person()
+            {
+                ARP = "AR2CH2GS11111"
+,
+                FirstName = "Carlo"
+,
+                LastName = "Spader"
+,
+                IsActive = true
+,
+                Authenticated = true
+            };
+
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            HttpContext.Current.Response.Write(js.Serialize(person));
+        }
+
+    }
+}
